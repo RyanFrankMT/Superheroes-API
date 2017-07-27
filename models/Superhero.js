ca1a6981f905c5387db089dcba8fb27644393d39
Ryan Frank <ryanfrank@Ryans-MacBook-Pro.local>
2017-07-26 19:23:51 -0600

    created Superhero schema

---
+++ models/Superhero.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const SuperheroSchema = new mongoose.Schema({
+  name: String,
+  superpower: String,
+  image: String,
+})
+
+module.exports = mongoose.model('Superhero',SuperheroSchema);
+// The above code is how we can export and share with other folders within this project
+// 'Superhero' in the line above is the name in which we will continue to use to call our schema
