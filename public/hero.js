8c519e091f1cb4b697b7cc4ac41ca215ddceadd8
Ryan Frank <ryanfrank@Ryans-MacBook-Pro.local>
2017-08-01 21:43:16 -0600

    added edit form to superheroes

--- public/hero.js
+++ public/hero.js
@@ -10,20 +10,23 @@ const app = new Vue({
     name: '',
     superpower: '',
     image: '',
+    selectedHero: undefined,
+    heroIndex: undefined,
+    showEdit: false
   },
   created(){
     this.loadData()
   },
   methods: {
-    loadData: function(){
-      let self = this
+    loadData(){
+      const self = this
 
       $.ajax({
         url: '/api/superheroes',
         method: 'GET'
       }).done((response) => self.heroes = response.data)
     },
-    createHero: function(){
+    createHero(){
       let self = this
       let newHero = {name: this.name,
                     superpower: this.superpower,
@@ -35,6 +38,37 @@ const app = new Vue({
         method: 'POST',
         data: newHero
       }).done((response) => console.log(response))
+    },
+    deleteHero(hero_id){
+      const self = this
+
+      $.ajax({
+        url: `/api/superheroes/${hero_id}`,
+        method: 'DELETE'
+      }).done((response) => self.loadData())
+    },
+    toggleEdit(hero_id, index){
+      this.showEdit = !this.showEdit
+      this.selectedHero = (this.showEdit) ? hero_id : undefined
+      this.heroIndex = (this.showEdit) ? index: undefined
+    },
+    editHero(){
+      const self = this
+
+      const modifiedHero = {
+        name: (self.name.length > 0) ? self.name : null,
+        superpower: (self.superpower.length > 0) ? self.superpower: null,
+        image: (self.image.length > 0) ? self.image : null
+      }
+      console.log('wheres the error')
+      $.ajax({
+        url: `/api/superheroes/${self.selectedHero}`,
+        method: 'PUT',
+        data: modifiedHero
+      }).done((response) => {
+        self.heroes[self.heroIndex] = response.data;
+        self.showEdit = false
+      })
     }
   }
 })
